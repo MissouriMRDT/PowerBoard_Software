fd386526cbc1c0d2cde51e78c5c49aac17dfb5fa
Jacob Lipina <jrlwd5@mst.edu>
2017-03-20 13:45:27 -0500

    Communication over Serial with BMS Added
    The power board can now recieve information from the BMS and pass along
    commands to BMS from base station. PB can recieve values for pack
    current, voltage check array, voltage check out, and voltage of each of
    the 8 cells. The commands that base station can now give BMS are battery
    pack off, batery pack reset, battery fans on, batt fans off, and sound
    BMS buzzer. This file only allows to communicate over Serial7 but
    Rovecomm is too be added in next.

--- PowerBoardSoftware/PowerBoardSoftware.ino
+++ PowerBoardSoftware/PowerBoardSoftware.ino
@@ -36,11 +36,10 @@ const uint16_t LOGIC_12V_CURRENT_READING    = 1116; //changed name; ID CORRECT
 const uint16_t COM_12V_CURRENT_READING      = 1117; //created new; ID CORRECT
 const uint16_t PACK_VOLTAGE_READING         = 1120; //created new; ID CORRECT
 
-const uint16_t ROVER_POWER_RESET           = 1041;
-
+const uint16_t ROVER_POWER_RESET            = 1041;
 const uint16_t POWER_BUS_ENABLE             = 1088;
 const uint16_t POWER_BUS_DISABLE            = 1089;
-const uint16_t POWER_BUS_OVER_CURRENT       = 1090;
+const uint16_t POWER_BUS_OVER_CURRENT       = 1090;   
 
 const uint8_t BUS_M1_ON_OFF                 = 0;
 const uint8_t BUS_M2_ON_OFF                 = 1;
@@ -56,7 +55,28 @@ const uint8_t BUS_12V_COM_ON_OFF            = 10; //created new
 const uint8_t BUS_12V_COM_LOGIC_ON_OFF      = 11; //created new
 const uint8_t FAN_ON_OFF                    = 12; //created new
 
-const int ROVER_POWER_RESET_DELAY          = 3000;
+/*const uint16_t BMS_PACK_CURRENT             = 1072;
+const uint16_t BMS_PACK_OVER_CURRENT        = ID;  //notification sent if pack overcurrents and shuts down
+const uint16_t BMS_V_CHECK_ARRAY            = ID;
+const uint16_t BMS_V_CHECK_OUT              = ID;
+const uint16_t BMS_UNDER_VOLTAGE            = ID;  //notification sent if a cell drops below a specified voltage. Value indicates cell
+const uint16_t CELL_1_VOLTAGE               = 1056;
+const uint16_t CELL_2_VOLTAGE               = 1057;
+const uint16_t CELL_3_VOLTAGE               = 1058;
+const uint16_t CELL_4_VOLTAGE               = 1059;
+const uint16_t CELL_5_VOLTAGE               = 1060;
+const uint16_t CELL_6_VOLTAGE               = 1061;
+const uint16_t CELL_7_VOLTAGE               = 1062;
+const uint16_t CELL_8_VOLTAGE               = 1063;
+
+const uint16_t BMS_CNTRL                    = ID;
+const uint8_t BATT_PACK_OFF                = 13;
+const uint8_t BATT_PACK_RESET              = 14;
+const uint8_t BATT_FANS_ON                 = 15;
+const uint8_t BATT_FANS_OFF                = 16;
+const uint8_t BMS_SOUND_BUZZER             = 17;*/
+
+const int ROVER_POWER_RESET_DELAY           = 3000;
 
 
 //Rovecomm :: RED packet :: data_id and data_value with number of data bytes size
@@ -116,6 +136,8 @@ const float SENSOR_BIAS          = VCC * SENSOR_SCALE;
 const float CURRENT_MAX          = (VCC - SENSOR_BIAS) / SENSOR_SENSITIVITY;
 const float CURRENT_MIN          = -SENSOR_BIAS / SENSOR_SENSITIVITY;
 float current_reading            = 0;
+bool com_over_current            = 0;
+float time1                      = 0;
 
 const float VOLTS_MIN            = 0;
 const float VOLTS_MAX            = 40;
@@ -127,6 +149,27 @@ const int DEBOUNCE_DELAY = 10;
 const int ESTOP_12V_COM_LOGIC_MAX_AMPS_THRESHOLD = 5;
 const int ESTOP_12V_EXTRA_ACT_MAX_AMPS_THRESHOLD = 15;   
 const int ESTOP_MOTOR_BUS_MAX_AMPS_THRESHOLD = 22;
+
+//Data from BMS
+//uint8_t pack_current_byte[4];
+//uint8_t v_check_array_byte[4];
+//uint8_t v_check_out_byte[4];
+uint8_t cell_voltages_byte[12];
+float cell_voltages[8]; //voltages for each cell
+//float pack_current = -1;
+//float v_check_array = -1; //pack voltage at 
+//float v_check_out = -1;
+
+union txable_float {
+    float f;
+    unsigned char ch[4];
+};
+
+union txable_float v_check_array;
+union txable_float v_check_out;
+union txable_float pack_current;
+
+
 
 // Checks the pin for bouncing voltages to avoid false positives
 bool singleDebounce(int bouncing_pin, int max_amps_threshold)
@@ -179,6 +222,21 @@ void setup()
 
   pinMode(FAN_CNTRL, OUTPUT);
   
+  digitalWrite(EXTRA_CNTRL, LOW);
+  digitalWrite(ACT_CNTRL, LOW);
+  digitalWrite(COM_LOGIC_CNTRL, LOW);
+  digitalWrite(COM_CNTRL, LOW);
+  digitalWrite(LOGIC_CNTRL, LOW);
+    
+  digitalWrite(M1_CNTRL, LOW);
+  digitalWrite(M2_CNTRL, LOW);
+  digitalWrite(M3_CNTRL, LOW);
+  digitalWrite(M4_CNTRL, LOW);
+  digitalWrite(M5_CNTRL, LOW);
+  digitalWrite(M6_CNTRL, LOW);
+  digitalWrite(M7_CNTRL, LOW);
+
+  digitalWrite(FAN_CNTRL, LOW);
   // Turn on everything when we begin
   delay(ROVER_POWER_RESET_DELAY);
   
@@ -199,6 +257,9 @@ void setup()
   digitalWrite(FAN_CNTRL, HIGH);
   
   roveComm_Begin(192, 168, 1, 132);
+
+  Serial.begin(9600);
+  Serial7.begin(115200);
 }//end setup
 
 //Loop
@@ -235,9 +296,20 @@ void loop()
     (POWER_BUS_OVER_CURRENT, sizeof(BUS_12V_COM_ON_OFF), &BUS_12V_COM_ON_OFF);
     delay(500);
     digitalWrite(COM_CNTRL, LOW);
+    time1 = millis();
+    com_over_current = 1;
     delay(ROVECOMM_DELAY);
   }//end if
   
+  if(com_over_current = 1)
+  {  
+    if(millis()>=(time1+10000))
+      {
+        digitalWrite(COM_CNTRL,HIGH);
+        com_over_current = 0;
+      }
+  }
+
   if( singleDebounce(M1_AMPS, ESTOP_MOTOR_BUS_MAX_AMPS_THRESHOLD) ) 
   {
     digitalWrite(M1_CNTRL, LOW);
@@ -288,7 +360,7 @@ void loop()
   }//end if
   
   /////////////////////////////////////////////RED Control and Telem RoveComm
-
+  
   //If there is no message, data_id gets set to zero
   roveComm_GetMsg(&data_id, &data_size, &data_value);
   switch (data_id) //either 0 or 1 
@@ -357,8 +429,8 @@ void loop()
           //Serial.println(data);
           break; 
      }//endswitch 
-     break;  
-   
+     //break;  
+ 
     case POWER_BUS_DISABLE: //data_id id 1089
           switch (data_value)
           { 
@@ -418,7 +490,7 @@ void loop()
               break; 
          }//endswitch 
          break;
-     
+
     case ROVER_POWER_RESET: //data_id is 1041
       
       digitalWrite(M1_CNTRL, LOW);
@@ -455,9 +527,7 @@ void loop()
 
       digitalWrite(FAN_CNTRL, HIGH);
       break;
-       
-     
-
+      
     /*case '5': //added for testing so I can ask for current reading on a specific bus.
 
       switch (incomingByte2)          /// (data_value)
@@ -587,11 +657,41 @@ void loop()
      }//endswitch 
      break;*/
 
+    /*case BMS_CNTRL: //data_id is ...
+          switch (data_value)
+          {
+            case BATT_PACK_OFF:
+              Serial7.write(1);  //if correct, make #s into constants
+              break;
+
+            case BATT_PACK_RESET:
+              Serial7.write(2);
+              break;
+
+            case BATT_FANS_ON:
+              Serial7.write(3);
+              break;
+
+            case BATT_FANS_OFF:
+              Serial7.write(4);
+              break;
+
+            case BMS_SOUND_BUZZER:
+              Serial7.write(5);
+              break;
+
+            default:
+              break;
+          }//endswitch
+          break;*/
+
+    
+
     default:
       //Serial.println("Unrecognized data_id: 5");
       //Serial.println(data_id);
       break;
-  }//endswitch 
+  }//endswitch
 
   adc_reading = analogRead(EXTRA_AMPS);
   current_reading = mapFloats(adc_reading, ADC_MIN, ADC_MAX, CURRENT_MIN, CURRENT_MAX);
@@ -652,6 +752,199 @@ void loop()
   voltage_reading = scale(adc_reading, ADC_MIN, ADC_MAX, VOLTS_MIN, VOLTS_MAX);
   roveComm_SendMsg(PACK_VOLTAGE_READING, sizeof(voltage_reading), &voltage_reading);
   delay(ROVECOMM_DELAY);
+
+  ////////////// BMS Communication /////////////////////////////////////////////////////////
+  //Serial7.println("Before if statement");
+
+  if (Serial7.available() >= 24) //number of bytes I expect to recieve from bms  //should this be Serial7.available???
+  { 
+    //Serial7.println("You made it inside!");
+    for (int i=0; i < 4; i++)
+    {
+      pack_current.ch[i] = Serial7.read(); 
+    }
+
+    for (int i=0; i < 4; i++)
+    {
+      Serial.write(pack_current.ch[i]);
+    }
+
+             //assuming least significant byte first
+    //pack_current = ((pack_current_byte[0]) | (pack_current_byte[1] << 8) | (pack_current_byte[2] << 16) | (pack_current_byte[3] <<24));
+   // roveComm_SendMsg(BMS_PACK_CURRENT, sizeof(pack_current), &pack_current);
+   // delay(ROVECOMM_DELAY);
+
+    for (int i=0; i < 4; i++)
+    {
+      v_check_array.ch[i] = Serial7.read();
+    }       
+    
+    for (int i=0; i < 4; i++)
+    {
+      Serial.write(v_check_array.ch[i]);
+    }
+    //v_check_array = ((v_check_array_byte[0]) | (v_check_array_byte[1] << 8) | (v_check_array_byte[2] << 16) | (v_check_array_byte[3] <<24));
+   // roveComm_SendMsg(BMS_V_CHECK_ARRAY, sizeof(v_check_array), &v_check_array);
+   // delay(ROVECOMM_DELAY);
+
+    for (int i=0; i < 4; i++)
+    {
+      v_check_out.ch[i] = Serial7.read();
+    }       
+    
+    for (int i=0; i < 4; i++)
+    {
+      Serial.write(v_check_out.ch[i]);
+    }  
+    //v_check_out = ((v_check_out_byte[0]) | (v_check_out_byte[1] << 8) | (v_check_out_byte[2] << 16) | (v_check_out_byte[3] <<24));
+    //roveComm_SendMsg(BMS_V_CHECK_OUT, sizeof(v_check_out), &v_check_out);
+    //delay(ROVECOMM_DELAY);
+  
+    for (int i=0; i < 12; i++)
+    {
+      cell_voltages_byte[i] = Serial7.read();
+    }   
+
+     for (int i=0; i < 3; i++)
+    {
+      Serial.write(cell_voltages_byte[i]);
+    }   
+  
+    //The CVR0x registers, as read in from SPI, are 8 bits, but the actual voltages are 12-bit floats
+    // spread out across multiple registers. Consult the LTC6803 datasheet (table 8, p. 23)
+    //
+    //The "split" occurs at the most significant nybble, then at the least significant, then most again, etc.
+    //You could probably put this in a cute little for loop, but I don't care yet.
+    cell_voltages[0] = cell_voltages_byte[0] | ((cell_voltages_byte[1] & 0x0F) << 8); //Cell 1
+    //roveComm_SendMsg(CELL_1_VOLTAGE, sizeof(cell_voltages[0]), &cell_voltages[0]);
+    //delay(ROVECOMM_DELAY);
+
+    cell_voltages[1] = ((cell_voltages_byte[1] & 0xF0) >> 4) | (cell_voltages_byte[2] << 4); //Cell 2
+   // roveComm_SendMsg(CELL_2_VOLTAGE, sizeof(cell_voltages[1]), &cell_voltages[1]);
+   // delay(ROVECOMM_DELAY);
+
+    cell_voltages[2] = cell_voltages_byte[3] | ((cell_voltages_byte[4] & 0x0F) << 8); //Cell 3
+  // roveComm_SendMsg(CELL_3_VOLTAGE, sizeof(cell_voltages[2]), &cell_voltages[2]);
+    //delay(ROVECOMM_DELAY);
+
+    cell_voltages[3] = ((cell_voltages_byte[4] & 0xF0) >> 4) | (cell_voltages_byte[5] << 4); //Cell 4
+    //roveComm_SendMsg(CELL_4_VOLTAGE, sizeof(cell_voltages[3]), &cell_voltages[3]);
+    //delay(ROVECOMM_DELAY);
+
+    cell_voltages[4] = cell_voltages_byte[6] | ((cell_voltages_byte[7] & 0x0F) << 8); //Cell 5
+   // roveComm_SendMsg(CELL_5_VOLTAGE, sizeof(cell_voltages[4]), &cell_voltages[4]);
+   // delay(ROVECOMM_DELAY);
+
+    cell_voltages[5] = ((cell_voltages_byte[7] & 0xF0) >> 4) | (cell_voltages_byte[8] << 4);//Cell 6
+    //roveComm_SendMsg(CELL_6_VOLTAGE, sizeof(cell_voltages[5]), &cell_voltages[5]);
+    //delay(ROVECOMM_DELAY);
+
+    cell_voltages[6] = cell_voltages_byte[9] | ((cell_voltages_byte[10] & 0x0F) << 8); //Cell 7
+    //roveComm_SendMsg(CELL_7_VOLTAGE, sizeof(cell_voltages[6]), &cell_voltages[6]);
+    //delay(ROVECOMM_DELAY);
+
+    cell_voltages[7] = ((cell_voltages_byte[10] & 0xF0) >> 4) | (cell_voltages_byte[11] << 4);//Cell 8
+   // roveComm_SendMsg(CELL_8_VOLTAGE, sizeof(cell_voltages[7]), &cell_voltages[7]);
+    //delay(ROVECOMM_DELAY);
+
+
+    //These still aren't normal voltages after putting them in floats; you need to do some extra processing.
+    for(int k = 0; k < 8; k++)
+      {
+        cell_voltages[k] -= 512;
+        cell_voltages[k] *= 1.5 * .001;//I don't yet know if this is correct; taken wholesale from solar car*/
+      }
+
+    //what should i look for to tell me to send a message to base station if pack overcurrents or undervoltages? Also, if pack is shut off by BMS code,
+    //will I even be able to send a message to base? PB wouldn't have power would it? If pack power is cut off, only BMS is on inside the rover.
+
+    //Serial.println("GET READY YA'LL!");
+    //Serial7.println("Incoming readings");
+    /*char incomingByte;
+    incomingByte = Serial.read();
+    switch (incomingByte)
+      case '1':
+      {*/
+      //}
+    Serial.print("Pack Current: ");
+    Serial.println(pack_current.f);
+
+    Serial.print("v_check_array: ");
+    Serial.println(v_check_array.f);
+
+    Serial.print("v_check_out: ");
+    Serial.println(v_check_out.f);
+
+    Serial.print("Cell 1 Voltage: ");
+    //Serial.println(cell_voltages[0]);
+    Serial.println((uint8_t)cell_voltages[0]);
+
+    Serial.print("Cell 2 Voltage: ");
+    //Serial.println(cell_voltages[1]);
+    Serial.println((uint8_t)cell_voltages[1]);
+
+    Serial.print("Cell 3 Voltage: ");
+    //Serial.println(cell_voltages[2]);
+    Serial.println((uint8_t)cell_voltages[2]);
+
+    Serial.print("Cell 4 Voltage: ");
+    //Serial.println(cell_voltages[3]);
+
+    Serial.print("Cell 5 Voltage: ");
+    //Serial.println(cell_voltages[4]);
+
+    Serial.print("Cell 6 Voltage: ");
+    //Serial.println(cell_voltages[5]);
+
+    Serial.print("Cell 7 Voltage: ");
+    //Serial.println(cell_voltages[6]);
+
+    Serial.print("Cell 8 Voltage: ");
+    //Serial.println(cell_voltages[7]);
+       
+    //Serial7.println("end");
+  }
+  //Serial7.println("Outside if");
+  //black to tx
+
+  char incomingByte;
+  if(Serial.available() > 0)
+  {  
+      incomingByte = Serial.read();
+      switch (incomingByte)
+      {
+        case '1'://BATT_PACK_OFF
+          Serial7.write(1);  //if correct, make #s into constants
+          Serial.println("Batt pack off");
+          break;
+
+        case '2'://BATT_PACK_RESET:
+          Serial7.write(2);
+          Serial.println("Batt pack reset");
+          break;
+
+        case '3'://BATT_FANS_ON:
+          Serial7.write(3);
+          Serial.println("Batt fans on");
+          break;
+
+        case '4'://BATT_FANS_OFF:
+          Serial7.write(4);
+          Serial.println("Batt fans off");
+          break;
+
+        case '5'://BMS_SOUND_BUZZER:
+          Serial7.write(5);
+          Serial.println("BMS sound buzzer");
+          break;
+
+        default:
+          break;
+      }//endswitch
+  }
+
+
+
 }//end loop
 
 
