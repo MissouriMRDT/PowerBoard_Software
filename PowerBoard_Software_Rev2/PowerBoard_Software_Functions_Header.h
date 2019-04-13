d6d5fef1dcfd59a615f31fdadf1e9d227705d77d
Evan Hite <Evan_Hite@github.com>
2019-04-13 09:23:14 -0500

    Initial Commit
    Updated the Rev1 branch with a struct to reduce the amount of  repeated functions.

--- PowerBoard_Software_Rev2/PowerBoard_Software_Functions_Header.h
+++ PowerBoard_Software_Rev2/PowerBoard_Software_Functions_Header.h
@@ -11,6 +11,22 @@ using namespace std ;
 
 RoveCommEthernetUdp RoveComm;
 
+//Struct For Motor Busses, Below are Global Constants
+struct PB_Bus
+{
+  uint8_t rovecomm_cell ; //Bit position in RoveComm packet, either 0 for ACL, or 1 for Motor Busses
+  uint8_t bit_code_off ; //Binary code with 0 being the position in which the bus exists in the rovecomm packet cell.
+  uint8_t bit_code_on ; //Binary code with 1 being the position in which the bus exists in the rovecomm packet cell.
+  int imeas_pin ; //current measurment pin on the board
+  int ctl_pin ; //control pin on the tiva
+  int amp_threshold ; //Amp Threshold on the bus
+  int bus_tuning ; //Tuning for each bus current reading
+  String identity ; //Identity of the bus  
+//Basically a constructor, but I do not need it for that purpose
+//I am simply wanting to be able to adjust all the values at once
+void Adjust_Values (const uint8_t & Rovecomm_cell, const uint8_t & Bit_code_off, const uint8_t & Bit_code_on, const int & Imeas_pin, const int & Ctl_pin, const int & Amp_threshold, const int & Bus_tuning, const String Identity) ;
+} ;
+
 //Global Constants
 
 //Delays
@@ -21,39 +37,39 @@ RoveCommEthernetUdp RoveComm;
 
 //////////////////////////////////////////////Pinmap
 // Control Pins for Busses
-#define ACT_CTL_PIN              PN_3
-#define LOGIC_CTL_PIN            PD_1
-#define COM_CTL_PIN              PH_2
-#define COM_LOGIC_CTL_PIN        PP_2
-#define AUX_CTL_PIN              PK_5
-#define M1_CTL_PIN               PK_7
-#define M2_CTL_PIN               PK_6
-#define M3_CTL_PIN               PH_1
-#define M4_CTL_PIN               PH_0
-#define M5_CTL_PIN               PM_2
-#define M6_CTL_PIN               PM_1
-#define M7_CTL_PIN               PM_0
-#define FAN_CTL_PIN              PM_3
+#define ACT_CTL_PIN              PN_3 //In rev 2
+#define LOGIC_CTL_PIN            PD_1 //In rev 2
+#define COMM_CTL_PIN             PH_2 //In rev 2
+#define COMM_LOGIC_CTL_PIN        PP_2 //In rev 2
+#define EM_CTL_PIN               PK_5 //Was Auxillary
+#define FM_CTL_PIN               PK_7 //Was M1
+#define MM_CTL_PIN               PK_6 //Was M2
+#define BM_CTL_PIN               PH_1 //Was M3
+#define AUX_CTL_PIN              PH_0 //Was M4
+#define ROCKET_CTL_PIN           PM_2 //Was M7
+#define GE_CTL_PIN               PM_1 //Was M6
+//#define M7_CTL_PIN               PM_0 //No longer in use
+#define FAN_CTL_PIN              PM_3 //In rev 2
 
 // Sensor Volts/Amps Readings Pins
-#define ACT_I_MEAS_PIN           PE_2
-#define LOGIC_I_MEAS_PIN         PE_1
-#define COM_I_MEAS_PIN           PE_0
-#define AUX_I_MEAS_PIN           PD_0
-#define M1_I_MEAS_PIN            PK_3
-#define M2_I_MEAS_PIN            PK_2
-#define M3_I_MEAS_PIN            PK_1 
-#define M4_I_MEAS_PIN            PK_0
-#define M5_I_MEAS_PIN            PB_5
-#define M6_I_MEAS_PIN            PB_4
-#define M7_I_MEAS_PIN            PE_3
-#define PACK_VOLTAGE_PIN         PE_5
+#define ACT_I_MEAS_PIN           PE_2 //In rev 2
+#define LOGIC_I_MEAS_PIN         PE_1 //In rev 2
+#define COMM_I_MEAS_PIN          PE_0 //In rev 2
+#define EM_I_MEAS_PIN            PD_0 //Was Auxillary in rev 1
+#define FM_I_MEAS_PIN            PK_3 //was M1
+#define MM_I_MEAS_PIN            PK_2 //was M2
+#define BM_I_MEAS_PIN            PK_1 //was M3
+#define AUX_I_MEAS_PIN           PK_0 //was M4
+#define ROCKET_I_MEAS_PIN        PB_5 //Was M5
+#define GE_I_MEAS_PIN            PB_4 //Was M6
+//#define M7_I_MEAS_PIN            PE_3 //No longer in use
+//#define PACK_VOLTAGE_PIN         PE_5 //No longer in use
 
 //////////////////////////////////////////////RoveBoard
 // Tiva1294C RoveBoard Specs
-#define VCC                  3300       //volts
+#define VCC                  3300      //volts
 #define ADC_MAX              4096      //bits
-#define ADC_MIN              420         //meme bits
+#define ADC_MIN              420       //meme bits
 
 //////////////////////////////////////////////Sensor
 // ACS722LLCTR-40AU-T IC Sensor Specs 
@@ -68,16 +84,22 @@ RoveCommEthernetUdp RoveComm;
 #define VOLTS_MAX             33600
 
 //Safest Test pin
-#define ESTOP_12V_COM_LOGIC_MAX_AMPS_THRESHOLD  2000 //5
-#define ESTOP_12V_ACT_MAX_AMPS_THRESHOLD        2000 //15  
-#define ESTOP_AUX_MAX_AMPS_THRESHOLD            17000 //17  (360W/21.6V) = 16.6 A
-#define ESTOP_MOTOR_BUS_MAX_AMPS_THRESHOLD      22000 //22
+#define ESTOP_12V_COMM_LOGIC_MAX_AMPS_THRESHOLD  2000 //2 amps, 5 amp fuse
+#define ESTOP_12V_ACT_MAX_AMPS_THRESHOLD        2000 //2 amps, 20 amp fuse  
+#define ESTOP_AUX_MAX_AMPS_THRESHOLD            17000 //17 amps, 20 amp fuse(360W/21.6V) = 16.6 A
+#define ESTOP_MOTOR_BUS_MAX_AMPS_THRESHOLD      30000 //30 amps, 40 amp fuse
+#define ESTOP_ROCKET_BUS_MAX_AMPS_THRESHOLD     2000 //2 amps, 5 amp fuse 
 
 //Tuning Variables
 #define LOGIC_COMM_TUNER  1085 //Tuner for ADC values for Logic and Comm busses
 #define ACT_TUNER         1132 //Tuner for ADC values for Actuation bus
 #define AUX_TUNER         1136 //Tuner for ADC values for Auxilliary bus
 #define MOTOR_TUNER       1144 //Tuner for ADC values for motor busses
+#define ROCKET_TUNER      1085 //Tuner for ADC values for Rocket antennas
+
+//Number of Busses
+#define ALC_BUSSES        3 //Number of Actuation, logic and Communication busses
+#define MOTOR_BUSSES      7 //Number of busses besides actuation, logic, and communication
 
 //Functions/////////////////////////////////////////////////////////////////////////
 
@@ -106,17 +128,22 @@ void Communication_Begin (uint8_t Bus []) ;
 //Pre: BUS_I_MEAS_PIN must be a pin for current measurement, Bus[] must be the same array from Commuincation_Begin.
 // BUS_CTL_PIN must be the relevent pin that controls the bus. and ESTOP_AMP_THRESHOLD must also be the relevent estop threshold.
 //Post: Shuts off the bus if there is an overcurrent situation and writes this to Bus[]
-bool Shut_Off( const int & BUS_I_MEAS_PIN, uint8_t Bus[], const int & BUS_CTL_PIN, const int & ESTOP_AMP_THRESHOLD, const int & Tuner) ;
+bool Shut_Off(const PB_Bus & Bus, uint8_t Send_Recieve[]) ;
 
 //Description: Takes a packet from RoveComm and turn on or off a bus
 //Pre: Enable_Disable should be a packet recieved from RoveComm, and Bus should be from Shut_Off
 //Post: Turns on or off all buses that the packet says should be on or off and writes this to Bus[].
-void Bus_Enable (const rovecomm_packet & Enable_Disable, uint8_t Bus[]) ;
+void Bus_Enable (const rovecomm_packet & Enable_Disable, uint8_t Send_Recieve[], const PB_Bus Bus[]) ;
 
 //Description: Reads a current sensing pin and saves this value to current_reading
 //Pre: Current_Reading should be an array part, for RoveComm reasons. BUS_I_MEAS_PIN should be a current measuring pin
 //that corresponds to the spot on the Current_Reading array.
 //Post: Returns a value to current_reading of the current reading value from the BUS_I_MEAS_PIN in mA.
 void Pin_Read (uint16_t & current_reading, const int & BUS_I_MEAS_PIN) ;
+
+//Description:
+//Pre:
+//Post:
+void Bus_Setup(PB_Bus Bus[]) ;
 
 #endif
