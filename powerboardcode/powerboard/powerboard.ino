00fd87059c076bc97cc077db276d6843ee3ab3e7
amwcandy <alex.m.wortmann@gmail.com>
2021-03-13 20:20:48 -0600

    started power board code

--- powerboardcode/powerboard/powerboard.ino
+++ powerboardcode/powerboard/powerboard.ino
@@ -1,43 +1,84 @@
 #include "powerboard.h"
-void setup() {
-
-  Serial.begin(9600);
-  setPins();
-  setPinStates();
+void setup() 
+{
+    Serial.begin(9600);
+    setPins();
+    setPinStates();
+    Serial.begin(9600);
+    RoveComm.begin(RC_POWERBOARD_FOURTHOCTET, &TCPServer);
+    delay(100);
 }
 
 void loop() 
 {
-  // put your main code here, to run repeatedly: 
-  
+    packet = RoveComm.read();
+    if(packet.data_id != 0)
+    {
+        switch(packet.data_id)
+        {
+            case RC_POWERBOARD_MOTORBUSENABLE_DATA_ID:
+                break;
+            case RC_POWERBOARD_12VACTBUSENABLE_DATA_ID:
+                break;
+            case RC_POWERBOARD_12VLOGICBUSENABLE_DATA_ID:
+                break;
+            case RC_POWERBOARD_30VBUSENABLE_DATA_ID:
+                break;
+            case RC_POWERBOARD_VACUUMENABLE_DATA_ID:
+                break;
+        }
+    }
+
 }
 
 void setPins()
 {
-  
-  for (int i = 0 ; i < 4 ; i++)
-  {
-    pinMode(currentSense[i], OUTPUT);
-  }
-
-  for (int i = 0 ; i < 11 ; i++)
-  {
-    pinMode(busses[i], INPUT);
-    pinMode(P_CurrentSense[i], OUTPUT);
-  }
-
-  for (int i = 0 ; i < 10 ; i++)
-  {
-    pinMode(P_busses[i], OUTPUT);
-  }
+    // sets input/output of pack busses and 12V current sense
+    for (int i = 0 ; i < 4 ; i++)
+    {
+        pinMode(currentSense12V[i], INPUT);
+        pinMode(bussesPack[i], OUTPUT);
+    }
+    // sets input/output of pack current sense and 12V busses
+    for (int i = 0 ; i < 11 ; i++)
+    {
+        pinMode(currentSensePack[i], INPUT);
+        pinMode(busses12V[i], OUTPUT);
+    }
+    // sets output of motor busses
+    for (int i = 0 ; i < 5 ; i++)
+    {
+        pinMode(bussesMotor[i], OUTPUT);
+    }
+    pinMode(drivePack, OUTPUT);
 }
 
 void setPinStates()
 {
-  // ONLY THESE FOR NOW BECAUSE THEY ARE THE ONLY FUNCTIONING ONES :)
-  for (int i = 3 ; i < 10 ; i++)
-  {
-    digitalWrite(busses[i], LOW);
-  }
-  
+    // turn on 12 volt busses
+    for (int i = 0 ; i < 11 ; i++)
+    {
+        digitalWrite(busses12V[i], HIGH);
+    }
+    // turns off motor busses
+    for (int i = 0 ; i < 5 ; i++)
+    {
+        digitalWrite(bussesMotor[i], LOW);
+    }
+    // turns on Pack busses
+    for (int i = 0 ; i < 4 ; i++)
+    {
+        digitalWrite(bussesPack[i], HIGH);
+    }
+    //turns off pack drive board bus
+    digitalWrite(drivePack, LOW);
+    delay(MOTOR_DELAY);
+    // turns on motor busses after a delay
+    for (int i = 0 ; i < 5 ; i++)
+    {
+        digitalWrite(bussesMotor[i], HIGH);
+    }
+    delay(DRIVE_DELAY);
+    // turns on pack drive bus
+    digitalWrite(drivePack, HIGH);
 }
